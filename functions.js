ce79cd3c601da3bbd16371464dd51a16861b0927
Charlie <charliemoomoo@yandex.com>
2024-08-17 21:01:49 +0800

    remove some unnecessarily promises
    and i dont want to modufy `taiwanearthquake.js` yet because that's too hard

--- functions.js
+++ functions.js
@@ -11,124 +11,108 @@ const weather = require("weather-js");
 const yts = require("yt-search");
 
 const Time = () => {
-  return new Promise((resolve, reject) => {
-    resolve({
-      name: "Time",
-      response: { content: moment().format("yyyy年MM月DD日 HH:mm:ss") },
-    });
-  });
+  return {
+    name: "Time",
+    response: { content: moment().format("yyyy年MM月DD日 HH:mm:ss") },
+  };
 };
-const MCJavaServer = (args) => {
-  return new Promise(async (resolve, reject) => {
-    const response = await fetch(`https://api.mcsrvstat.us/3/${args.server}`);
-    var body = await response.json();
-    if (body.icon) delete body.icon;
-    resolve({
-      name: "MCJavaServer",
-      response: { content: body },
-    });
-  });
+const MCJavaServer = async (args) => {
+  const response = await fetch(`https://api.mcsrvstat.us/3/${args.server}`);
+  var body = await response.json();
+  if (body.icon) delete body.icon;
+  return {
+    name: "MCJavaServer",
+    response: { content: body },
+  };
 };
-const MCBedrockServer = (args) => {
-  return new Promise(async (resolve, reject) => {
-    const response = await fetch(
-      `https://api.mcstatus.io/v2/status/bedrock/${args.server}`
-    );
-    const body = await response.json();
-    resolve({
-      name: "MCBedrockServer",
-      response: { content: body },
-    });
-  });
+const MCBedrockServer = async (args) => {
+  const response = await fetch(
+    `https://api.mcstatus.io/v2/status/bedrock/${args.server}`
+  );
+  const body = await response.json();
+  return {
+    name: "MCBedrockServer",
+    response: { content: body },
+  };
 };
 const Joke = () => {
-  return new Promise((resolve, reject) => {
-    resolve({
-      name: "Joke",
-      response: { content: jokes[Math.floor(Math.random() * jokes.length)] },
-    });
-  });
+  return {
+    name: "Joke",
+    response: { content: jokes[Math.floor(Math.random() * jokes.length)] },
+  };
 };
 const Google = async (args) => {
-  return new Promise(async (resolve, reject) => {
-    resolve({
-      name: "Google",
-      response: {
-        results: await google({ query: args.query, disableConsole: true }),
-      },
-    });
-  });
+  return {
+    name: "Google",
+    response: {
+      results: await google({ query: args.query, disableConsole: true }),
+    },
+  };
 };
 const Browser = async (args) => {
-  return new Promise(async (resolve, reject) => {
-    const response = await fetch(args.url);
-    const body = await response.text();
-    resolve({
-      name: "Browser",
-      response: { content: body },
-    });
-  });
+  const response = await fetch(args.url);
+  const body = await response.text();
+  return {
+    name: "Browser",
+    response: { content: body },
+  };
 };
 const Invoice = async () => {
-  return new Promise(async (resolve, reject) => {
-    const response = await fetch("https://invoice.etax.nat.gov.tw/invoice.xml");
-    const body = await response.text();
-    const resu = xml.xml2js(body, {
-      ignoreComment: true,
-      alwaysChildren: true,
-    });
-    const title =
-      resu.elements[0].elements[0].elements[4].elements[0].elements[0].cdata;
-    const content =
-      resu.elements[0].elements[0].elements[4].elements[3].elements[0].cdata.split(
-        "</p><p>"
-      );
-    content[0] = content[0].split("<p>")[1];
-    content[content.length - 1] = content[content.length - 1].split("</p>")[0];
-    resolve({
-      name: "Invoice",
-      response: { month: title, numbers: content },
-    });
+  const response = await fetch("https://invoice.etax.nat.gov.tw/invoice.xml");
+  const body = await response.text();
+  const resu = xml.xml2js(body, {
+    ignoreComment: true,
+    alwaysChildren: true,
   });
+  const title =
+    resu.elements[0].elements[0].elements[4].elements[0].elements[0].cdata;
+  const content =
+    resu.elements[0].elements[0].elements[4].elements[3].elements[0].cdata.split(
+      "</p><p>"
+    );
+  content[0] = content[0].split("<p>")[1];
+  content[content.length - 1] = content[content.length - 1].split("</p>")[0];
+  return {
+    name: "Invoice",
+    response: { month: title, numbers: content },
+  };
 };
 
 const GenerateImage = async (args) => {
-  return new Promise(async (resolve, reject) => {
-    const prompt = args.prompt || args.image;
-    if (!prompt)
-      return resolve({
-        name: "GenerateImage",
-        response: {
-          error:
-            'No prompt specified. Make sure to put your prompt in the "prompt" property.',
-        },
-      });
-    const Client = (await import("@gradio/client")).Client;
-    const client = await Client.connect(
-      "https://black-forest-labs-flux-1-schnell.hf.space",
-      {
-        hf_token: process.env.HF_ACCESS_TOKEN,
-      }
-    );
-    const result = (
-      await client.predict("/infer", {
-        prompt,
-        seed: args.seed || 0,
-        randomize_seed: !args.seed ? true : false,
-        width: args.width || 1024,
-        height: args.height || 1024,
-      })
-    ).data;
-    const id = result[0].path.split("/tmp/gradio/")[1].split("/")[0];
-    fs.writeFileSync(`images/${id}.webp`, await download(result[0].url));
-    resolve({
+  const prompt = args.prompt || args.image;
+  if (!prompt)
+    return {
       name: "GenerateImage",
       response: {
-        url: `https://cowai.cowgl.xyz/api/images/${id}.webp`,
-        seed: result[1],
+        error:
+          'No prompt specified. Make sure to put your prompt in the "prompt" property.',
       },
-    });
-  });
+    };
+  const Client = (await import("@gradio/client")).Client;
+  const client = await Client.connect(
+    "https://black-forest-labs-flux-1-schnell.hf.space",
+    {
+      hf_token: process.env.HF_ACCESS_TOKEN,
+    }
+  );
+  const result = (
+    await client.predict("/infer", {
+      prompt,
+      seed: args.seed || 0,
+      randomize_seed: !args.seed ? true : false,
+      width: args.width || 1024,
+      height: args.height || 1024,
+    })
+  ).data;
+  const id = result[0].path.split("/tmp/gradio/")[1].split("/")[0];
+  fs.writeFileSync(`images/${id}.webp`, await download(result[0].url));
+  return {
+    name: "GenerateImage",
+    response: {
+      url: `https://cowai.cowgl.xyz/api/images/${id}.webp`,
+      seed: result[1],
+    },
+  };
 };
 const GetWeather = async (args) => {
   return new Promise(async (resolve, reject) => {
@@ -160,47 +144,42 @@ const GetWeather = async (args) => {
   });
 };
 const SearchRepository = async (args) => {
-  return new Promise(async (resolve, reject) => {
-    const query = args.query || args.q;
-    if (!query)
-      return resolve({
-        name: "SearchRepository",
-        response: {
-          error:
-            'No query specified. Make sure to put your query in the "query" property.',
-        },
-      });
-    const { Octokit } = await import("@octokit/rest");
-    const octokit = new Octokit();
-    octokit.rest.search.repos({ q: query }).then(({ data }) => {
-      return resolve({
-        name: "SearchRepository",
-        response: {
-          result: data,
-        },
-      });
-    });
-  });
+  const query = args.query || args.q;
+  if (!query)
+    return {
+      name: "SearchRepository",
+      response: {
+        error:
+          'No query specified. Make sure to put your query in the "query" property.',
+      },
+    };
+  const { Octokit } = await import("@octokit/rest");
+  const octokit = new Octokit();
+  const { data } = await octokit.rest.search.repos({ q: query });
+  return {
+    name: "SearchRepository",
+    response: {
+      result: data,
+    },
+  };
 };
 const SearchVideo = async (args) => {
-  return new Promise(async (resolve, reject) => {
-    const query = args.query || args.q;
-    if (!query)
-      return resolve({
-        name: "SearchVideo",
-        response: {
-          error:
-            'No query specified. Make sure to put your query in the "query" property.',
-        },
-      });
-    const videos = (await yts(query)).videos;
+  const query = args.query || args.q;
+  if (!query)
     return resolve({
       name: "SearchVideo",
       response: {
-        result: videos,
+        error:
+          'No query specified. Make sure to put your query in the "query" property.',
       },
     });
-  });
+  const videos = (await yts(query)).videos;
+  return {
+    name: "SearchVideo",
+    response: {
+      result: videos,
+    },
+  };
 };
 
 const available_functions = {
