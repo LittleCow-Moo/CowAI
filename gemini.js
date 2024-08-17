b4a1231097bf20aee8d8a7e0845535772fbba108
Charlie <charliemoomoo@yandex.com>
2024-08-17 18:56:41 +0800

    BLOCK_NONE

--- gemini.js
+++ gemini.js
@@ -29,16 +29,16 @@ const model = genAI.getGenerativeModel(
     generationConfig: cow.config,
     tools: cow.tools,
     safetySettings: [
-      { category: "HARM_CATEGORY_HATE_SPEECH", threshold: "BLOCK_ONLY_HIGH" },
+      { category: "HARM_CATEGORY_HATE_SPEECH", threshold: "BLOCK_NONE" },
       {
         category: "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-        threshold: "BLOCK_ONLY_HIGH",
+        threshold: "BLOCK_NONE",
       },
       {
         category: "HARM_CATEGORY_DANGEROUS_CONTENT",
-        threshold: "BLOCK_ONLY_HIGH",
+        threshold: "BLOCK_NONE",
       },
-      { category: "HARM_CATEGORY_HARASSMENT", threshold: "BLOCK_ONLY_HIGH" },
+      { category: "HARM_CATEGORY_HARASSMENT", threshold: "BLOCK_NONE" },
     ],
   },
   requestOptions
