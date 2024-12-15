301817311ec54b14d4ff3b0e9175392a152249d4
Charlie <charliemoomoo@yandex.com>
2024-12-15 09:11:52 +0800

    im stupid, i forgot this

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ require("./gemini");
 if (process.env.ENABLE_DISCORD == "true") require("./discord");
 if (process.env.ENABLE_TELEGRAM == "true") require("./telegram");
 if (process.env.ENABLE_LINE == "true") require("./line");
+if (process.env.ENABLE_IRC == "true") require("./irc");
